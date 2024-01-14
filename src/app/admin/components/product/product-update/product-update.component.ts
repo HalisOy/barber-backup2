efcf11e26729e0b67f68105609f89c952c18d9e7
Halis <halisoy21@gmail.com>
2024-01-14 22:37:52 +0300

    Create Project

---
+++ src/app/admin/components/product/product-update/product-update.component.ts
@@ -0,0 +1,57 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Product, ProductWithCategory } from '../../../../models/product';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from '../../../../services/product.service';
+import { CategoryService } from '../../../../services/category.service';
+import { Category } from '../../../../models/category';
+
+@Component({
+  selector: 'app-product-update',
+  standalone: true,
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
+  templateUrl: './product-update.component.html',
+  styleUrl: './product-update.component.scss'
+})
+export class ProductUpdateComponent{
+    categories:Category[]=[]
+    updateForm!:FormGroup;
+    @Output() onLoad:EventEmitter<unknown>=new EventEmitter();
+    constructor(
+      private formBuilder:FormBuilder,
+      private toastrService:ToastrService,
+      private productService:ProductService,
+      private categoryService:CategoryService){}
+
+      getCategoryList(){
+        this.categoryService.getAll().subscribe(result=>{
+          this.categories=result.data
+        })
+      }
+    createUpdateForm(product:Product){
+      this.updateForm=this.formBuilder.group({
+        categoryId:[product.categoryId,Validators.required],
+        id:[product.id,Validators.required],
+        name:[product.name,Validators.required],
+        description:[product.description,Validators.required],
+        tags:[product.tags,Validators.required],
+        price:[product.price,Validators.required],
+        isActive:[product.isActive,Validators.required],
+      })
+    }
+    onSubmit(){
+      if(!this.updateForm.valid){
+        this.toastrService.warning("Please check the form.","Warning");
+        return;
+      }
+      let product:Product=Object.assign({},this.updateForm.value);
+      this.productService.update(product).subscribe(result=>{
+        if(typeof document ==undefined) return;
+        document.querySelector(".edit-modal")?.classList.toggle("show");
+        document.querySelector(".modal-backdrop")?.classList.toggle("show");
+        this.onLoad.emit();
+      })
+    }
+
+}
